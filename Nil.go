1df00db07600c668750a2eadfbf15d0d577de25a
Eduard Urbach <e.urbach@gmail.com>
2019-08-16 20:01:13 +0900

    Improved nil check

--- Nil.go
+++ Nil.go
@@ -1,12 +1,13 @@
 package assert
 
 import (
+	"reflect"
 	"testing"
 )
 
 // Nil asserts that the given parameter equals nil.
 func Nil(t *testing.T, a interface{}) {
-	if a == nil {
+	if isNil(a) {
 		return
 	}
 
@@ -16,10 +17,26 @@ func Nil(t *testing.T, a interface{}) {
 
 // NotNil asserts that the given parameter does not equal nil.
 func NotNil(t *testing.T, a interface{}) {
-	if a != nil {
+	if !isNil(a) {
 		return
 	}
 
 	t.Errorf("assert.NotNil:\nvalue: %v\nexpected: non-nil value", a)
 	t.FailNow()
+}
+
+// isNil returns true if the object is nil.
+func isNil(object interface{}) bool {
+	if object == nil {
+		return true
+	}
+
+	value := reflect.ValueOf(object)
+
+	switch value.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.UnsafePointer, reflect.Interface, reflect.Slice:
+		return value.IsNil()
+	}
+
+	return false
 }
